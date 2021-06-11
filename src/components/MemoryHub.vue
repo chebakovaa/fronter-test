a484c72704fa8f4c203017b7e221f732eafc14b8
Aleksey <chbkv@mail.ru>
2021-06-11 13:52:12 +0500

    add new component

---
+++ src/components/MemoryHub.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="task1-panel">
+        <div class="task1-panel-time">
+        </div>
+        <div class="task1-panel-board">
+        </div>
+    </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: {
+  },
+})
+export default class MemoryHub extends Vue {}
+
+</script>
+
+<style scoped>
+
+.task1-panel {
+    display: inline-flex;
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    align-items: stretch;
+    height: 500px;
+    background-color: aqua;
+}
+
+.task1-panel-time {
+    order: 1;
+}
+
+.task1-panel-board {
+    order: 2;
+}
+
+</style>
--- src/views/Memory.vue
+++ src/views/Memory.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="task1">
-
     </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import MemeryHub from "@/components/MemoryHub.vue";
 
 @Options({
   components: {
+    MemoryHub,
   },
 })
 export default class Memory extends Vue {}
