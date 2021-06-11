a9519b1beb1d12572495a2871acbaedf08fd84c7
Aleksey <chbkv@mail.ru>
2021-06-11 17:41:30 +0500

    trying to include css var

--- src/components/MemoryBoard.vue
+++ src/components/MemoryBoard.vue
@@ -1,5 +1,11 @@
 <template>
    <div>
+       <style>
+           :root {
+               --count-col: {{ countCol }};
+               --count-row: {{ countCol }};
+           }
+       </style>
    </div> 
 </template>
 
@@ -8,17 +14,21 @@
         width: 100%;
         height: 100%;
         display: grid;
-        grid-template-rows: repeat(3, auto [row-start]);
-        grid-template-columns: repeat(3, auto [col-start]);
+        grid-template-rows: repeat(var(--count-col), auto [row-start]);
+        grid-template-columns: repeat(var(--count-row), auto [col-start]);
     }
 </style>
 
 <script lang="ts">
-import { Options, Vue } from "vue-property-decorator";
+import { Options, Prop, Vue } from "vue-property-decorator";
 
 @Options({
   components: {
   },
 })
-export default class MemoryBoard extends Vue {}
+export default class MemoryBoard extends Vue {
+    @Prop({default: 8})
+    readonly countCol: number
+
+}
 </script>
