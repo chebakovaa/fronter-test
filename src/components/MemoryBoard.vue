5f24012380f7bfbdaa9840edbd12787451240b40
ЧебаковАА <achbkv@gmail.com>
2021-06-12 16:37:27 +0500

    Created component MemoryCell

--- src/components/MemoryBoard.vue
+++ src/components/MemoryBoard.vue
@@ -1,5 +1,6 @@
 <template>
-   <div :style="cssVars">
+   <div :style="cssVars" >
+     <MemoryCell v-for="cell in cells" :key="cell" :title="cell"/>
    </div> 
 </template>
 
@@ -10,14 +11,17 @@
         display: grid;
         grid-template-rows: repeat(var(--count-col), auto [row-start]);
         grid-template-columns: repeat(var(--count-row), auto [col-start]);
+        gap: 2px 2px;
     }
 </style>
 
 <script lang="ts">
 import { Options, Prop, Vue } from "vue-property-decorator";
+import MemoryCell from "@/components/MemoryCell.vue"
 
 @Options({
   components: {
+    MemoryCell,
   },
   computed: {
     cssVars() {
@@ -25,14 +29,18 @@ import { Options, Prop, Vue } from "vue-property-decorator";
         '--count-col': this.countCol,
         '--count-row': this.countCol,
       }
-    }
+    },
+    cells() {
+      return this.items;
+    },
   }
 })
 export default class MemoryBoard extends Vue {
+    
+    items: number[] = [...Array(64).keys()];
 
     @Prop({default: 8})
     readonly countCol!: number
 
-    
 }
 </script>
